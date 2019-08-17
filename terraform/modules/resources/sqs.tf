1aa089e053db4a7196b925d96af395c7da0050a3
Rafael Bertelli <rafael.bertelli@dosh.cash>
2019-08-17 13:49:53 -0300

    Terraforming SQS for credit & debit Ops

--- terraform/modules/resources/sqs.tf
+++ terraform/modules/resources/sqs.tf
@@ -1,5 +1,7 @@
 locals {
   transactionsQueueName = "transactions-${var.environment}"
+  creditCardOpsQueueName = "credit-card-ops-${var.environment}"
+  debitCardOpsQueueName = "debit-card-ops-${var.environment}"
 }
 
 resource "aws_sqs_queue" "transactions" {
@@ -13,4 +15,16 @@ resource "aws_lambda_event_source_mapping" "transaction-sqs-event-source" {
   enabled = true
   batch_size = 1
   event_source_arn = "${aws_sqs_queue.transactions.arn}"
+}
+
+resource "aws_sqs_queue" "creditCardOperationsQueue" {
+  name                       = "${local.creditCardOpsQueueName}"
+  visibility_timeout_seconds = 30
+  message_retention_seconds  = 86400
+}
+
+resource "aws_sqs_queue" "debitCardOperationsQueue" {
+  name                       = "${local.debitCardOpsQueueName}"
+  visibility_timeout_seconds = 30
+  message_retention_seconds  = 86400
 }
